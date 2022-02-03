2f83d6c33fe24b134634c924ab6351e645a95cbe
Koshi Futami <pizza.lover.kf@gmail.com>
2022-02-04 01:41:03 +0900

    運営者送信用の処理を修正

--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
 use App\Mail\ContactSendmail;
+use App\Mail\ContactAdminSendmail;
 
 class ContactController extends Controller
 {
