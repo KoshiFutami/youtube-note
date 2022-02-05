b99d27afb9d7d0ee8807f11cd809f2bc763e5a02
Koshi Futami <pizza.lover.kf@gmail.com>
2022-02-05 18:05:14 +0900

    ホーム画面に自分の最新の投稿を追加

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -15,11 +15,16 @@ class HomeController extends Controller
      */
     public function home()
     {
-        // 最新の投稿
-        $notes = Note::orderBy('created_at', 'DESC')->take(6)->get();
+        // 自分の最新のメモ
+        if (auth()->user()) {
+            $my_notes = Note::where('user_id', auth()->id())->orderBy('created_at', 'DESC')->take(6)->get();
+        }
+        
+        // 他のユーザーの最新のメモ
+        $others_notes = Note::where('user_id', '<>' ,auth()->id())->orderBy('created_at', 'DESC')->take(6)->get();
 
         // 人気のタグ
-        $popular_tags = Tag::withCount('notes')->orderBy('created_at', 'DESC')->take(3)->get();
+        $popular_tags = Tag::withCount('notes')->orderBy('notes_count', 'DESC')->take(3)->get();
 
         $popular_tags_notes = [];
         foreach ($popular_tags as $index => $tag) {
@@ -27,7 +32,8 @@ class HomeController extends Controller
         }
 
         return view('home', [
-            'notes' => $notes,
+            'my_notes' => $my_notes,
+            'others_notes' => $others_notes,
             'popular_tags' => $popular_tags,
             'popular_tags_notes' => $popular_tags_notes,
         ]);
