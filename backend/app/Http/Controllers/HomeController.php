2f2e3ee99c52d377a4e30a3899e3299648232bf5
Koshi Futami <pizza.lover.kf@gmail.com>
2022-02-08 01:24:03 +0900

    ホーム画面に表示するメモの数を3件ずつに変更

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -17,18 +17,18 @@ class HomeController extends Controller
     {
         // 自分の最新のメモ
         if (auth()->user()) {
-            $my_notes = Note::where('user_id', auth()->id())->orderBy('created_at', 'DESC')->take(4)->get();
+            $my_notes = Note::where('user_id', auth()->id())->orderBy('created_at', 'DESC')->take(3)->get();
         }
         
         // SOKOMIRUユーザーの最新のメモ
-        $users_notes = Note::orderBy('created_at', 'DESC')->take(4)->get();
+        $users_notes = Note::orderBy('created_at', 'DESC')->take(3)->get();
 
         // 人気のタグ
         $popular_tags = Tag::withCount('notes')->orderBy('notes_count', 'DESC')->take(3)->get();
 
         $popular_tags_notes = [];
         foreach ($popular_tags as $index => $tag) {
-            $popular_tags_notes[$index] = $tag->notes()->orderBy('created_at', 'DESC')->take(4)->get();
+            $popular_tags_notes[$index] = $tag->notes()->orderBy('created_at', 'DESC')->take(3)->get();
         }
         if (auth()->user()) {
             return view('home', [
