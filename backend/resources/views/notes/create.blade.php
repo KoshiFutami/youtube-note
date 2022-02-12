f370f6606eb6c90afd61d619c24a43a338f73b1e
Koshi Futami <pizza.lover.kf@gmail.com>
2022-02-13 01:38:27 +0900

    タグ内のスペースの不具合を修正

--- backend/resources/views/notes/create.blade.php
+++ backend/resources/views/notes/create.blade.php
@@ -12,14 +12,14 @@
                 @csrf
                 <div class="form__head">新しいメモを作成</div>
                 <div class="form__item">
-                    <label for="title">タイトル<span　class="is-required">※必須</span></label>
+                    <label for="title">タイトル<span class="is-required">※必須</span></label>
                     <input type="text" name="title" id="title" class="{{ $errors->has('title') ? 'is-invalid' : '' }}" value="{{ old('title') }}">
                     @if ($errors->has('title'))
                     <div class="form__error">{{ $errors->first('title') }}</div>
                     @endif
                 </div>
                 <div class="form__item">
-                    <label for="video_url">YouTube動画のURL<span　class="is-required">※必須</span></label>
+                    <label for="video_url">YouTube動画のURL<span class="is-required">※必須</span></label>
                     <input type="text" name="video_url" id="video_url" class="{{ $errors->has('video_url') ? 'is-invalid' : '' }}" value="{{ old('video_url') }}">
                     @if ($errors->has('video_url'))
                     <div class="form__error">{{ $errors->first('video_url') }}</div>
@@ -27,20 +27,20 @@
                 </div>
                 <div class="form__item">
                     <label for="start_seconds">再生位置<span class="instruction">（時:分:秒）</span></label>
-                    <input type="number" min="0"　name="start_seconds_h" id="start_seconds_h" class="{{ $errors->has('start_seconds_h') ? 'is-invalid' : '' }}" value="{{ old('start_seconds_h', 0) }}"> : <input type="number" min="0" max="59"　name="start_seconds_m" id="start_seconds_m" class="{{ $errors->has('start_seconds_m') ? 'is-invalid' : '' }}" value="{{ old('start_seconds_m', 0) }}"> : <input type="number" min="0" max="59"　name="start_seconds_s" id="start_seconds_s" class="{{ $errors->has('start_seconds_s') ? 'is-invalid' : '' }}" value="{{ old('start_seconds_s', 0) }}">
+                    <input type="number" min="0" name="start_seconds_h" id="start_seconds_h" class="{{ $errors->has('start_seconds_h') ? 'is-invalid' : '' }}" value="{{ old('start_seconds_h', 0) }}"> : <input type="number" min="0" max="59" name="start_seconds_m" id="start_seconds_m" class="{{ $errors->has('start_seconds_m') ? 'is-invalid' : '' }}" value="{{ old('start_seconds_m', 0) }}"> : <input type="number" min="0" max="59" name="start_seconds_s" id="start_seconds_s" class="{{ $errors->has('start_seconds_s') ? 'is-invalid' : '' }}" value="{{ old('start_seconds_s', 0) }}">
                 @if ($errors->has('start_seconds'))
                     <div class="text-danger">{{ $errors->first('start_seconds') }}</div>
                 @endif
                 </div>
                 <div class="form__item">
                     <label for="tags">タグ<span class="instruction">（複数のタグを追加する場合にはスペースで区切ってください）</span></label>
-                    <input type="text" name="tags" id="tags" class="{{ $errors->has('tags') ? 'is-invalid' : '' }}"　value="{{ old('tags') }}" placeholder="新規タグ">
+                    <input type="text" name="tags" id="tags" class="{{ $errors->has('tags') ? 'is-invalid' : '' }}" value="{{ old('tags') }}" placeholder="新規タグ">
                 @if ($errors->has('tags'))
                     <div class="form__error">{{ $errors->first('tags') }}</div>
                 @endif
                 </div>
                 <div class="form__item">
-                    <label for="content">メモ内容<span　class="is-required">※必須</span></label>
+                    <label for="content">メモ内容<span class="is-required">※必須</span></label>
                     <textarea type="text" name="content" id="content" class="{{ $errors->has('content') ? 'is-invalid' : '' }}">{{ old('content') }}</textarea>
                     @if ($errors->has('content'))
                     <div class="form__error">{{ $errors->first('content') }}</div>
